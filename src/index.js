fb60207ae2bd8ec05d3120137945c71923993c3f
ddeleon267 <dominique.a.deleon@gmail.com>
2021-06-15 11:59:08 -0400

    Adjusts code to be more broken.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("#create-task-form");
+  
+  form.addEventListener("submit", function (event) {
+    const todoItem = event.value
+    const todolist = document.getElementById("tasks")
+    const lineitem = document.createElement("li")
+    lineitem.innerText = `#{todoItem}`
+  });
 });
