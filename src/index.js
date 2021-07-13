0a1c755d28789613c8cf55bca632e5d5e8bd82d8
ddeleon267 <dominique.a.deleon@gmail.com>
2021-07-13 10:57:29 -0400

    Updates broken code.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector("#create-task-form");
+  var form = document.querySelector("#create-task-form");
   
   form.addEventListener("submit", function (event) {
-    const todoItem = event.value
-    const todolist = document.getElementById("tasks")
-    const lineitem = document.createElement("li")
+    var todoItem = event.value
+    var todolist = document.getElementById("tasks")
+    var lineitem = document.createElement("li")
     lineitem.innerText = `#{todoItem}`
   });
 });
+
+
+
+// Bonus:
+// A delete function that will remove tasks from your list
+// A priority value selected from a dropdown that is used to determine the color of the text in the list(e.g.red for high priority, yellow for medium, green for low)
+//   As a challenge, implement a sorting functionality that displays the tasks ascending or descending order based on priority
+// An additional input field(e.g.user, duration, date due)
+// Ability to edit tasks
+// Something of your choice! The main objective is to add a feature that allows the user's input to affect the DOM
