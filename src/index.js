e315c8ef70b079eb33639f8b1cf3975b94c0ef7f
Dominique DeLeon <dominique.a.deleon@gmail.com>
2023-03-07 21:38:55 -0500

    Adds updated functonal solution.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,50 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var form = document.querySelector("#create-task-form");
-  
-  form.addEventListener("submit", function (event) {
-    var todoItem = event.value
-    var todolist = document.getElementById("tasks")
-    var lineitem = document.createElement("li")
-    lineitem.innerText = `#{todoItem}`
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  //grabs all the necessary DOM elements.
+
+  //set the taskForm to a variable.
+  const newTaskForm = document.querySelector('form')
+
+  //attach a submit event listener to taskForm.
+  newTaskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //create a variable called toDo holds the value of the targeted input.
+    const newToDo = e.target.newtaskdescription.value
+
+    //pass in the toDo variable as an argument to buildToDo. 
+    buildToDo(newToDo);
+
+    //after buildToDo function runs, we then reset the newTaskForm so the input is empty
+    newTaskForm.reset()
+  })
 });
 
+  //when user submits, we pass in the targeted input into buildToDo function
+  function buildToDo(newToDo){
+    //first, the buildToDo function creates an li element and a button element. 
+    let taskLi = document.createElement('li');
+    let deleteButton = document.createElement('button')
 
+    //next, we set the textContent of the btn element to 'x'.
+    deleteButton.textContent = 'x'
 
-// Bonus:
-// A delete function that will remove tasks from your list
-// A priority value selected from a dropdown that is used to determine the color of the text in the list(e.g.red for high priority, yellow for medium, green for low)
-//   As a challenge, implement a sorting functionality that displays the tasks ascending or descending order based on priority
-// An additional input field(e.g.user, duration, date due)
-// Ability to edit tasks
-// Something of your choice! The main objective is to add a feature that allows the user's input to affect the DOM
+    //after, we set the textContent of the li element to the todo parameter.
+    //remember, when we submit, we are passing in targeted input into the parameters. 
+    taskLi.textContent = `${newToDo}`
+
+    //next, we set the text content for the li and button element, we then append the new btn to the li.
+    taskLi.appendChild(deleteButton);
+
+    //then, we added the li element to the ul element with id of tasks
+    document.querySelector('#tasks').appendChild(taskLi);
+  
+  
+    //finally, we attached a click event listener to the btn which invokes the handleDelete function
+    deleteButton.addEventListener('click', handleDelete)
+  }
+
+  //when the button element with the textContent of 'x' is clicked, this function will run.
+  function handleDelete(e){
+    //this function targets the parent node of the button and removes it.
+    //in this case, the parentNode of the button element is the li element. Which means that the li will then be removed form list.
+    e.target.parentNode.remove();
+  }
